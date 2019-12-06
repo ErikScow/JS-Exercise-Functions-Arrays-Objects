19d118e49943f7a930a59a77e03736a8d5073441
Erik Scow <erikscow@gmail.com>
2019-12-05 18:27:02 -0800

    completed final mvp requirements

--- index.js
+++ index.js
@@ -246,8 +246,23 @@ function getOlderCars(inventory, maxYear) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory) {
+  let germanCars = [];
+  for (let i = 0; i < inventory.length; i++){
+    if (inventory[i].car_make === 'Audi') {
+      germanCars.push(inventory[i]);
+    }
+    else if (inventory[i].car_make === 'Mercedes-Benz') {
+      germanCars.push(inventory[i]);
+    }
+    else if (inventory[i].car_make === 'Volkswagen') {
+      germanCars.push(inventory[i]);
+    }
+    else if (inventory[i].car_make === 'BMW') {
+      germanCars.push(inventory[i]);
+    }
+  }
+  return germanCars;
 }
 
 /**
@@ -268,9 +283,9 @@ function getGermanCars(/* code here */) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum  = (a,b) => a+b;
+const addFive = (num) => num+5;
+const argTimesTwo = (num) => num*2;
 
 /**
  * ### Challenge `carMaker`
